61a9cb36a0ddff12139735bc111827c98e9b426d
Terry <gongte1937@gmail.com>
2025-08-07 19:52:10 +0930

    initialize the app

---
+++ .DbSchema/samples/Northwind-schema.sql
@@ -0,0 +1,13 @@
+1. Steuer
+2. Kirchensteuer
+
+
+1 Chart
+2. 2checkout test order, preferred db
+3. Apple issue
+4. Close Alexa
+5. Cristi vxvdata, winningstarted
+6. Cassandra explain plan. Move driver back
+7. Oracle PDB - schema from Ionut - speed test
+8. Testat download driver - permissions, Firewall, etc.
+9. Fix sample project
